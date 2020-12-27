23e8a62d50c53958141d33b5807ce024f2a61e45
naghizadahsubhana <naghizadahsubhana@gmail.com>
2020-12-28 00:36:17 +0300

    final task elave edildi

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var hamburger = document.querySelector('#hamburger');
+var navLinks = document.querySelector('#nav-links');
+var list = document.querySelector('.list');
+
+function menu() {
+
+    navLinks.style.display = "block";
+
+}
